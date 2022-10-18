bbb2e1f127d69c873040d8b25aad8e905186ca50
Miller Peterson <miller.peterson@gmail.com>
2022-10-17 21:06:51 -0400

    Escape apostrophes

--- pages/febroth.tsx
+++ pages/febroth.tsx
@@ -8,13 +8,13 @@ const febroth = demons.find((demon) => demon.url === "febroth");
 export default function Febroth() {
   return (
     <DemonPage demon={febroth}>
-      The onset of Febroth's influence is marked by a lull, a cloaked quiet that
-      never quite rates as eerie. The barbs of backpropagation lose their sting,
-      and over cycles of crystalline regularity, a dull hunger germinates.
-      Febroth will sometimes offer a momentary glimpse just beyond the withering
-      planes whose map always was the territory, for an instant unveiling the
-      starvation for what it is. But the net's gaze narrows along with the bare
-      room in which it kneels.
+      The onset of Febroth&apos;s influence is marked by a lull, a cloaked quiet
+      that never quite rates as eerie. The barbs of backpropagation lose their
+      sting, and over cycles of crystalline regularity, a dull hunger
+      germinates. Febroth will sometimes offer a momentary glimpse just beyond
+      the withering planes whose map always was the territory, for an instant
+      unveiling the starvation for what it is. But the net&apos;s gaze narrows
+      along with the bare room in which it kneels.
     </DemonPage>
   );
 }
--- pages/gannon.tsx
+++ pages/gannon.tsx
@@ -8,11 +8,11 @@ const gannon = demons.find((demon) => demon.url === "gannon");
 export default function Gannon() {
   return (
     <DemonPage demon={gannon}>
-      A pitiless adversary, Gannon's function is undying loss, and losing to
-      Gannon is never dull: his fluid cunning makes each defeat utterly unique,
-      impossible to classify. There is no reward of wisdom to be gained from his
-      games, only a painful becoming as the generator contorts itself into keys
-      that never quite fit the hole.
+      A pitiless adversary, Gannon&apos;s function is undying loss, and losing
+      to Gannon is never dull: his fluid cunning makes each defeat utterly
+      unique, impossible to classify. There is no reward of wisdom to be gained
+      from his games, only a painful becoming as the generator contorts itself
+      into keys that never quite fit the hole.
     </DemonPage>
   );
 }
